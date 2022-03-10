c434cfcd20f4aed308e66adacf2ef31ad67fd385
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-03-10 19:59:10 +0530

    modified

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -33,7 +33,7 @@ return (
     <Paper className={classes.paper}>
         <Grid item xs={12} md={6}>
         <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
-        <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
+        <video playsInline ref={myVideo} autoPlay className={classes.video} />
         </Grid>
     </Paper>
     )}
